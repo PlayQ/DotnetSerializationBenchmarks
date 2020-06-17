7ba0ce0c94fc034bc1c98c85153713c88e421b83
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2020-06-17 11:33:31 +0300

    fix bson serialization.

--- Common.cs
+++ Common.cs
@@ -9,12 +9,14 @@ namespace c_sharp_serialization_bench
     [MessagePackObject]
     public sealed class NodeMeta
     {
+        [BsonConstructor("MetaInfo")]
         public NodeMeta(Dictionary<string, string> meta)
         {
             MetaInfo = meta;
         }
 
         [Key(0)]
+        [BsonElementAttribute]
         public Dictionary<string, string> MetaInfo { get; }
     }
 
@@ -22,6 +24,7 @@ namespace c_sharp_serialization_bench
     [MessagePackObject]
     public sealed class NodeBase
     {
+        [BsonConstructor("Hidden", "Id", "Name", "Meta")]
         public NodeBase(bool hidden, string id, string name, NodeMeta meta)
         {
             Hidden = hidden;
@@ -30,12 +33,16 @@ namespace c_sharp_serialization_bench
             Meta = meta;
         }
         [Key(0)]
+        [BsonElementAttribute]
         public bool Hidden { get; }
         [Key(1)]
+        [BsonElementAttribute]
         public string Id { get; }
         [Key(2)]
+        [BsonElementAttribute]
         public string Name { get; }
         [Key(3)]
+        [BsonElementAttribute]
         public NodeMeta Meta { get; }
     }
 
@@ -48,24 +55,28 @@ namespace c_sharp_serialization_bench
     [MessagePackObject]
     public class NVar : Node
     {
+        [BsonConstructor("Value")]
         public NVar(double value)
         {
             Value = value;
         }
 
         [Key(0)]
+        [BsonElementAttribute]
         public double Value { get; }
     }
 
     [MessagePackObject]
     public class NGroup : Node
     {
+        [BsonConstructor("Value")]
         public NGroup(NodeGroup value)
         {
             Value = value;
         }
 
         [Key(0)]
+        [BsonElementAttribute]
         public NodeGroup Value { get; }
     }
 
@@ -73,6 +84,7 @@ namespace c_sharp_serialization_bench
     public sealed class NodeGroup
     {
         [JsonConstructor]
+        [BsonConstructor("Base", "Nodes")]
         public NodeGroup(NodeBase nodeBase, List<Node> nodes)
         {
             Base = nodeBase;
@@ -86,8 +98,10 @@ namespace c_sharp_serialization_bench
         }
 
         [Key(0)]
+        [BsonElementAttribute]
         public NodeBase Base { get; }
         [Key(1)]
+        [BsonElementAttribute]
         public List<Node> Nodes { get; }
     }
 }
